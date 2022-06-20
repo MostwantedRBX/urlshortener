5362ca96d81e28a0d5e8a81f2c3d74bf1eb54319
MostwantedRBX <mostwantedrbxsteam@gmail.com>
2022-06-20 15:50:17 -0500

    batch file to build react project and mv it to src

---
+++ buildweb.bat
@@ -0,0 +1,42 @@
+@ECHO off
+
+REM This bat file will build the React Frontend code in /web-src/, rename it to web, and place it into the /src/ directory.
+
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+SET scriptDir = %~dp0
+
+ECHO "Building React Project..."
+CD ".\web-src\"
+CALL npm run build
+ECHO "Done Building - Renaming folder"
+RENAME build web
+CD "..\src\"
+DEL ".\web"
+CD "..\web-src\"
+MOVE ".\web" "..\src\"
+CD ".."
